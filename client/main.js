c16097980a715dad1e49646bf4768d38cbf9c23d
juwon <juwon0481@gmail.com>
2024-07-03 22:02:50 +0900

    - javascript webComponent Part

--- client/main.js
+++ client/main.js
@@ -1,44 +1,25 @@
 
 
 
-import { 
-  getNode,
-  getStorage,
-  setStorage,
-  deleteStorage,
-  clearContents,
- } from "./lib/index.js";
+import { TodoList } from './components/TodoList/TodoList.js'
+import { TodoItem } from './components/TodoItem/TodoItem.js'
 
 
-
- const textField = getNode('#textField');
- const clear = getNode('button[data-name="clear"]');
+const app = document.getElementById('app');
 
 
- getStorage('text')
- .then((res)=>{
-   textField.value = res;
- })
-
- function handleTextField(){
-
-  const value = this.value;
-
-  setStorage('text',value);
+const defineElements = () => {
+  customElements.define('todo-app',TodoList);
+  customElements.define('todo-item',TodoItem);
+}
 
 
- }
-
- function handleClear(){
-  deleteStorage('text');
-  clearContents(textField);
- }
-
- textField.addEventListener('input',handleTextField);
- clear.addEventListener('click',handleClear);
+defineElements()
 
 
+const todoElement = document.createElement('todo-app');
 
+app.append(todoElement);
 
 
 
