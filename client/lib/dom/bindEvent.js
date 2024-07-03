7d1d6c7734ce50006500be5dc26838bd820b199d
juwon <juwon0481@gmail.com>
2024-07-03 21:54:51 +0900

    - Javascript Calculator Part

--- client/lib/dom/bindEvent.js
+++ client/lib/dom/bindEvent.js
@@ -1,8 +1,9 @@
+import { getNode } from "./getNode.js";
+import { isString } from "../utils/type.js";
 
 
 
-
-function bindEvent(node,type,handler){
+export function bindEvent(node,type,handler){
   
   if(isString(node)) node = getNode(node);
 
--- client/lib/dom/css.js
+++ client/lib/dom/css.js
@@ -1,4 +1,5 @@
-
+import { getNode } from "./getNode.js";
+import { isString, isObject, isArray } from "../utils/type.js";
 
 
 /* -------------------------------------------- */
@@ -13,7 +14,7 @@
  * @returns {void}
  */
 
-function addClass(node,...className){
+export function addClass(node,...className){
 
   if(typeof node === 'string') node = document.querySelector(node)
   
@@ -36,9 +37,7 @@ function addClass(node,...className){
     
 }
 
-
-
-function removeClass(node,className){
+export function removeClass(node,className){
 
   if(typeof node === 'string') node = document.querySelector(node)
 
@@ -54,7 +53,7 @@ function removeClass(node,className){
   node.classList.remove(className);
 }
 
-function toggleClass(node,className){
+export function toggleClass(node,className){
   if(typeof node === 'string') node = document.querySelector(node)
 
   if(typeof className !== 'string'){
@@ -96,7 +95,7 @@ function setStyle(node,prop,value){
   
 }
 
-const css = (node,prop,value) => !value ? getStyle(node,prop) : setStyle(node,prop,value);
+export const css = (node,prop,value) => !value ? getStyle(node,prop) : setStyle(node,prop,value);
 
 
 
